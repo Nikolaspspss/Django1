bb0688d3542e7c19e8697b3cd7c49f48bdfb2ef9
Name <email@example.ru>
2022-09-13 15:38:02 +0300

    1

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', include('django.contrib.flatpages.urls')),
+    path('start/', include('django.contrib.flatpages.urls')),
 ]
